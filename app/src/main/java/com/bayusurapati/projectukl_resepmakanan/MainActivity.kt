ad20f24aa8bd4db1b8d2898769c06c76252fb699
BayuSurapati <nyomanbayu.surapati@gmail.com>
2023-02-08 08:55:30 +0700

    UKL_Resep

--- app/src/main/java/com/bayusurapati/projectukl_resepmakanan/MainActivity.kt
+++ app/src/main/java/com/bayusurapati/projectukl_resepmakanan/MainActivity.kt
@@ -2,10 +2,23 @@ package com.bayusurapati.projectukl_resepmakanan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportActionBar?.setLogo(R.drawable.arrowback_icon)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_atas, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
